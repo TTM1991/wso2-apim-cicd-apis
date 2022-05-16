58ad88af2b37cafe235e41eccb158da03ebef3d5
thilinitm <98460047+thilinitm@users.noreply.github.com>
2022-05-16 17:44:05 +0530

    changes

--- config.sh
+++ config.sh
@@ -7,18 +7,18 @@ rm -rf $HOME/.wso2apictl
 
 echo 'setting up dev environment'
 apictl add-env -e dev \
-                    --registration https://dev.apim.wso2.com/client-registration/v0.15/register \
-                    --apim https://dev.apim.wso2.com \
-                    --token https://dev.apim.wso2.com/token \
-                    --admin https://dev.apim.wso2.com/api/am/admin/v0.15 \
-                    --api_list https://dev.apim.wso2.com/api/am/publisher/v0.15/apis \
-                    --app_list https://dev.apim.wso2.com/api/am/store/v0.15/applications
+                    --registration https://localhost:9444/client-registration/v0.17/register \
+                    --apim https://localhost:9444 \
+                    --token https://localhost:9444/token \
+                    --admin https://localhost:9444/api/am/admin/v0.17 \
+                    --api_list https://localhost:9444/api/am/publisher/v0.17/apis \
+                    --app_list https://localhost:9444/api/am/store/v0.17/applications
 
 echo 'setting up prod environment'
 apictl add-env -e prod \
-                    --registration https://prod.apim.wso2.com/client-registration/v0.15/register \
-                    --apim https://prod.apim.wso2.com \
-                    --token https://prod.apim.wso2.com/token \
-                    --admin https://prod.apim.wso2.com/api/am/admin/v0.15 \
-                    --api_list https://prod.apim.wso2.com/api/am/publisher/v0.15/apis \
-                    --app_list https://prod.apim.wso2.com/api/am/store/v0.15/applications
+                    --registration https://localhost:9443/client-registration/v0.17/register \
+                    --apim https://localhost:9443 \
+                    --token https://localhost:9443/token \
+                    --admin https://localhost:9443/api/am/admin/v0.17 \
+                    --api_list https://localhost:9443/api/am/publisher/v0.17/apis \
+                    --app_list https://localhost:9443/api/am/store/v0.17/applications
